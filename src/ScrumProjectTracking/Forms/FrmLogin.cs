b052c2bc5088dfa2155b9f533406b3dce4a7a898
S S <spshrout@ius.edu>
2019-03-04 11:41:57 -0500

    Resource file updates

--- src/ScrumProjectTracking/Forms/FrmLogin.cs
+++ src/ScrumProjectTracking/Forms/FrmLogin.cs
@@ -7,11 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Crypt = BCrypt.Net.BCrypt;
+using ScrumProjectTracking.DataAccess;
+using ScrumProjectTracking.Forms;
 
 namespace ScrumProjectTracking
 {
+    
+
     public partial class FrmLogin : Form
     {
+
+        bool closedCorrectly = false;
         public FrmLogin()
         {
             InitializeComponent();
@@ -22,24 +29,46 @@ namespace ScrumProjectTracking
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void passwordLabelLogin_Click(object sender, EventArgs e)
         {
 
         }
 
         private void loginButton_Click(object sender, EventArgs e)
         {
+            string userN = loginUsername.Text;
+            IDataAccess dc = new ScrumDBSource();
+            var userNFromTable = from u in dc.Users where u.UserID == userN select u.UserID;
 
+            if (userNFromTable.FirstOrDefault() != null)
+            {     
+                string passW = loginPassword.Text;
+                var hashTest = (from u in dc.Users where u.UserID == userN select u.PasswordHash).ToArray();
+                if (Crypt.Verify(passW, hashTest[0]))
+                {
+                    Console.WriteLine("Test");
+                    closedCorrectly = true;
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Error: The provided password was incorrect. Passwords are case-sensitive.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error: The provided username was not found. Usernames are case-sensitive.");
+            }
+
+
+        }
+
+        private void FrmLogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!closedCorrectly)
+            {
+                Application.Exit();
+            }
         }
     }
 }
